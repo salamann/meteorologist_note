29ab43b6edee9d450a3e14c53dac50ce10ff0880
Shunyo Ogawa <shunyo44@gmail.com>
2021-02-20 16:29:00 +0900

    today's study

--- source/conf.py
+++ source/conf.py
@@ -76,7 +76,8 @@ math_number_all = True
 def setup(app):
     app.add_stylesheet('block.css')
     app.add_config_value('recommonmark_config', {
-        'url_resolver': lambda url: github_doc_root + url,
+        'enable_math': True,
+        'enable_inline_math': True,
         'auto_toc_tree_section': 'Contents',
     }, True)
     app.add_transform(AutoStructify)
